d5938bb976c4d8f39805b0efe52788e68ee903ae
Joshua Coleman <jlcolema@icloud.com>
2024-08-29 14:43:57 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# MASS Energy Lab Insulation
---
+++ dev/html/a/js/functions.js
@@ -0,0 +1,99 @@
+
+/*-------------------------------------------
+	Browser Detection
+-------------------------------------------*/
+
+// For when you get desparate.
+
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
+/*-------------------------------------------
+	General Functions
+-------------------------------------------*/
+
+
+(function($){
+
+
+	/* On Page Ready */
+
+	$(document).ready(function (){
+
+
+		/*-------------------------------------------
+			Title
+		-------------------------------------------*/
+
+		// Notes...
+
+
+		/*-------------------------------------------
+			Screen Size
+		-------------------------------------------*/
+
+
+		// Add class of "dev" to <body>
+
+
+		// $("body").addClass("dev");
+
+
+		// Displays screen size on the fly.
+
+
+		// var windowSize = $(window).width();
+
+		// $("#footer").after('<div id="dev"></div>');
+
+		// $("#dev").text(windowSize);
+
+
+	});
+
+
+	/* On Page Load */
+
+
+	$(window).load(function() {
+
+
+		/*-------------------------------------------
+			Title
+		-------------------------------------------*/
+
+		// Notes...
+
+
+	});
+
+
+	/* On Window Resize */
+
+
+	$(window).resize(function() {
+
+
+		/*-------------------------------------------
+			Screen Size
+		-------------------------------------------*/
+
+
+		// Displays screen size on the fly.
+
+
+		// var windowSize = $(window).width();
+
+		// $("#dev").text(windowSize);
+
+
+	});
+
+
+})(window.jQuery);
